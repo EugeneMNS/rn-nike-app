65e4d37d49797d196d10ea300af3ec8b79af0ca9
EugeneMNS <evgen.str4@gmail.com>
2023-04-30 15:30:54 +0300

    feat: add Products Details Screen

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import {StyleSheet, View} from 'react-native';
-import {ProductsScreen} from "./src/screens/ProductsScreen";
+import {ProductDetailsScreen} from "./src/screens/ProductDetailsScreen";
 
 export default function App() {
     return (
         <View style={styles.container}>
-            <ProductsScreen/>
+            <ProductDetailsScreen/>
         </View>
     );
 }
@@ -13,7 +13,5 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
     },
 });
