94d285abd2f987d0ee28b8fee3dac3cbc8d8b877
EugeneMNS <evgen.str4@gmail.com>
2023-05-01 11:59:26 +0300

    fix: cart slice

--- src/store/cartSlice.js
+++ src/store/cartSlice.js
@@ -12,14 +12,14 @@ export const cartSlice = createSlice({
     reducers: {
         addCartItem: (state, action) => {
             const newProduct = action.payload.product;
-            const cartItem = state.items.find((item) => item.product.id === newProduct.id)
+            const cartItem = state.items.find(
+                (item) => item.product.id === newProduct.id
+            );
             if (cartItem) {
                 cartItem.quantity += 1;
             } else {
-                state.items.push({ product: newProduct, quantity: 1 })
+                state.items.push({ product: newProduct, quantity: 1 });
             }
-
-            state.items.push({product: newProduct, quantity: 1})
         },
         changeQuantity: (state, action) => {},
     }
